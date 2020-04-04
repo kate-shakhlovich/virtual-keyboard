236667bf032e91aebd09146235721b4e0d018eaf
Kate Shakhlovich <kate.shakhlovich@gmail.com>
2020-04-04 13:52:13 +0300

    fix: Backspace and Delete

--- js/keyboard/keyboard.js
+++ js/keyboard/keyboard.js
@@ -29,17 +29,13 @@ export default class Keyboard {
 
     attachEvents() {
         document.addEventListener('keydown', (e) => {
-            this.isShifted = e.shiftKey;
-            this.isAlted = e.altKey;
             this.el.classList.toggle("keyboard_isShifted", e.shiftKey);
         });
 
         document.addEventListener('keyup', (e) => {
-            if(this.isShifted && this.isAlted && (e.key == "Shift" || e.key == "Alt")) {
+            if(e.key == "Control" && e.altKey || e.key == "Alt" && e.ctrlKey) {
                 this.language = this.language === LANGUAGE.EN ? LANGUAGE.RU : LANGUAGE.EN;
             }
-            this.isShifted = e.shiftKey;
-            this.isAlted = e.altKey;
             this.el.classList.toggle("keyboard_isShifted", e.shiftKey);
         });
 
@@ -55,14 +51,29 @@ export default class Keyboard {
             this.outputEl.focus();
         }
         
-        this.buttons.find(x => x.code === "Backspace").el.onclick = (e) => {
-            this.outputEl.value = this.outputEl.value.substring(0, this.outputEl.value.length - 1);
+        const removeCharacter = (position) => {
+            const { value, selectionStart, selectionEnd } = this.outputEl;
+            // const value = this.outputEl.value;
+            // const selectionStart = this.outputEl.selectionStart;
+            // const selectionEnd = this.outputEl.selectionEnd;
             this.outputEl.focus();
+            
+            if (selectionStart === selectionEnd) {
+                this.outputEl.value = value.substring(0, position) + value.substring(position + 1);
+                this.outputEl.setSelectionRange(position, position);
+            } else {
+                this.outputEl.value = value.substring(0, selectionStart) + value.substring(selectionEnd);
+                this.outputEl.setSelectionRange(selectionStart, selectionStart);
+            }
+            
+        }
+
+        this.buttons.find(x => x.code === "Backspace").el.onclick = (e) => {
+            removeCharacter(this.outputEl.selectionStart - 1);
         }
 
         this.buttons.find(x => x.code === "Delete").el.onclick = (e) => {
-            this.outputEl.value = this.outputEl.value.substring(1);
-            this.outputEl.focus();
+            removeCharacter(this.outputEl.selectionStart);
         }
     }
 
